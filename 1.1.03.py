b182d4b87a66ed14eb9c6c389da2aada690accc8
Boris Kin <nicko@sibnet.ru>
2025-04-12 19:10:55 +0700

    Цикл while

---
+++ 1.1.03.py
@@ -0,0 +1,7 @@
+n, count = int(input()), 0
+
+while n > 0:
+    count += 1
+    n -= 5
+
+print(count)
---
+++ 1.1.04.py
@@ -0,0 +1,8 @@
+n, result = int(input()), 0
+
+while n != 0:
+    result += n
+    n = int(input())
+
+
+print(result)
